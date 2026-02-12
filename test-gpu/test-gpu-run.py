9d38adef3ab7748e927ce782340f9c13fd1e9ed3
Kristian Ehlert <kristian.ehlert@gmail.com>
2026-02-12 16:16:00 +0100

    feat: are there actually gpus

--- test-gpu/test-gpu-run.py
+++ test-gpu/test-gpu-run.py
@@ -8,6 +8,8 @@ def gpu_task(gpu_id):
     if torch.cuda.is_available():
         x = torch.rand(1000, 1000).cuda()
         return f"GPU sum: {x.sum().item():.2f}"
+    else:
+        raise Exception(f"No GPUS!")
     return "No GPU"
 
 ray.get([gpu_task.remote(i) for i in range(2)])
