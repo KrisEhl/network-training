7ab0944f37eb1a2f10322292b8f93d3d99371399
Kristian Ehlert <kristian.ehlert@gmail.com>
2026-02-12 15:04:06 +0100

    feat: provide port and ip as environment variables

--- register-host.sh
+++ register-host.sh
@@ -1,3 +1,9 @@
 # uv run ray metrics launch-prometheus # not working correctly
 echo RAY_ENABLE_WINDOWS_OR_OSX_CLUSTER=$RAY_ENABLE_WINDOWS_OR_OSX_CLUSTER
-RAY_ENABLE_WINDOWS_OR_OSX_CLUSTER=1 uv run ray start --head --port=6379 --dashboard-host=0.0.0.0 --dashboard-port=8265
+echo Registering on RAY_API_SERVER_IP=$RAY_API_SERVER_IP with RAY_API_SERVER_PORT=$RAY_API_SERVER_PORT
+RAY_ENABLE_WINDOWS_OR_OSX_CLUSTER=1 uv run ray start \
+                            --head \
+                            --port=$RAY_API_SERVER_PORT \
+                            --node-ip-address $RAY_API_SERVER_IP \
+                            --dashboard-host=0.0.0.0 \
+                            --dashboard-port=8265
